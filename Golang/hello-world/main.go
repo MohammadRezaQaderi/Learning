de56a0f1ab44923ef2980eaa220adf66b086ab8b
MohammadReza Qaderi <mrq112775@outlook.com>
2022-01-02 10:37:09 +0330

    Start with go hello go:)

---
+++ Golang/hello-world/main.go
@@ -0,0 +1,32 @@
+package main
+
+
+
+import "fmt"
+
+
+// these are global variables which are accessible from functions
+
+
+const (
+  c1 =  10.1
+  c2 int64 = 11
+)
+
+var v1 = 10
+var v2 uint64 = 10
+
+
+func  main(){
+	x := 10 // it`s like this var x = 10
+
+	// Or we can don`t have initial value
+	var y int
+	fmt.Println(c1)
+	fmt.Println(c2)
+	fmt.Printf("%d\n", y)
+	fmt.Printf("%d %d\n", v1, v2)
+	fmt.Println(x)
+}
+
+
