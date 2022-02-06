9040ccf3f81eb18c7bb7a4e36d69729d43524746
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-02-06 22:27:43 +0330

    the slice in go instade of array

---
+++ Golang/07-slices/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	s1 := make([]int, 10)
+
+	s1[0] = 10
+	s1[1] = 20
+	s1[2] = 30
+
+	fmt.Printf("s1: %+v (len(s1): %d, (cap(s1)): %d \n", s1, len(s1), cap(s1))
+
+	for index, value := range s1 {
+		fmt.Printf("s1[%d]: %d", index, value)
+	}
+	fmt.Println()
+
+	s2 := make([]int, 0, 10)
+	fmt.Printf("s2: %+v (len(s2): %d, (cap(s2)): %d \n", s2, len(s2), cap(s2))
+
+	s1 = append(s1, 27)
+	fmt.Printf("s1: %+v (len(s1): %d, (cap(s1)): %d \n", s1, len(s1), cap(s1))
+
+	s2 = append(s2, 27)
+	fmt.Printf("s2: %+v (len(s2): %d, (cap(s2)): %d \n", s2, len(s2), cap(s2))
+
+}
