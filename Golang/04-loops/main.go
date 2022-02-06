7fe7fc8b68d818911dfe6cd0c66d8ad5a8efd024
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-02-06 21:38:21 +0330

    the loops in go

--- Golang/04-loops/main.go
+++ Golang/04-loops/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func IsPrime(n int) bool {
 	for i := 2; i < n; i++ {
 		if n%i == 0 {
@@ -13,7 +15,7 @@ func IsPrime(n int) bool {
 func NthPrime(n int) int {
 	i := 2
 	counter := 2
-
+	// unbounded for
 	for {
 		if IsPrime(i) {
 			counter++
@@ -28,7 +30,21 @@ func NthPrime(n int) int {
 }
 func GCD(n, m int) int {
 	for n%m != 0 {
-		n, m = m, n%m
+		n, m = m, n%m // multiple assignment
 	}
 	return m
+}
+
+func main() {
+	var n int
+
+	fmt.Printf("please enter the number: \n")
+	//call by ref to edit n
+	fmt.Scanf("%d", &n)
+
+	fmt.Printf("is %d prime? %t \n", n, IsPrime(n))
+	fmt.Printf("%d is  %d prime \n", n, NthPrime(n))
+
+	fmt.Println("gcd", 10, 20, "is", GCD(10, 20))
+	fmt.Println("gcd", 13, 15, "is", GCD(13, 15))
 }
