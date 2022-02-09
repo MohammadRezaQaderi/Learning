df9b998fa4b0d3e760620f4df11095cdb03f95cb
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-02-09 16:24:29 +0330

    the echo simple server

---
+++ Golang/echo/handler/hello.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/MohammadRezaQaderi/Learning/Golang/echo/request"
+	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	From string
+}
+
+func (h Hello) User(c echo.Context) error {
+	value := c.Param("username")
+
+	log.Println(value)
+
+	// nolint: wrapcheck
+	return c.NoContent(http.StatusNoContent)
+}
+
+func (h Hello) Get(c echo.Context) error {
+	if value := c.QueryParam("hello"); value != "" {
+		log.Println(value)
+	}
+
+	return c.JSON(http.StatusOK, fmt.Sprintf("Hello To you from %s", h.From))
+}
+
+func (h Hello) Post(c echo.Context) error {
+	var req request.Name
+
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if req.Count == nil {
+		log.Println("There is no Count")
+	} else {
+		log.Printf("There is a count %d", *req.Count)
+	}
+
+	// nolint: wrapcheck
+	return c.String(http.StatusOK, fmt.Sprintf("Hello to %s from %s", req.Name, h.From))
+}
---
+++ Golang/echo/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/MohammadRezaQaderi/Learning/Golang/echo/handler"
+	"github.com/labstack/echo/v4"
+	"log"
+)
+
+func main() {
+	e := echo.New()
+	h := handler.Hello{
+		From: "DORDOR",
+	}
+	e.GET("/hello", h.Get)
+	e.POST("/hello", h.Post)
+	e.GET("/hello/:username", h.User)
+
+	if err := e.Start("127.0.0.1:1378"); err != nil {
+		log.Fatal(err)
+	}
+}
