813e68b8671c3c8e53c3b4f73fd1970725d7c610
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-02-19 09:06:22 +0330

    use sync wait group to manage threads

--- Golang/booking-app/main.go
+++ Golang/booking-app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/Learning/Golang/booking-app/helper"
+	"sync"
 	"time"
 )
 
@@ -19,36 +20,42 @@ type UserData struct {
 	numberOfTickets uint
 }
 
+var wg = sync.WaitGroup{}
+
 func main() {
 
 	fmt.Printf("Welcome to %v booking aplication\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, RemainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	for {
-		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, RemainingTickets)
-		if isValidName && isValidEmail && isValidTicketNumber {
-			bookTicket(userTickets, firstName, lastName, email)
-			go sendTicket(userTickets, firstName, lastName, email)
-			firstNames := getFirstName()
-			fmt.Printf("These are all our booking: %v\n", firstNames)
-			if RemainingTickets == 0 {
-				fmt.Println("Our conference is booked out. Come back next year.")
-				break
-			}
-		} else {
-			if !isValidName {
-				fmt.Println("Your entered Name is very short")
-			}
-			if !isValidEmail {
-				fmt.Println("your email address is not correct")
-			}
-			if !isValidTicketNumber {
-				fmt.Printf("We have only %v tickets remaining, so you can't book %v tickets\n", RemainingTickets, userTickets)
-			}
+	//for {
+	firstName, lastName, email, userTickets := getUserInput()
+	isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, RemainingTickets)
+	if isValidName && isValidEmail && isValidTicketNumber {
+		bookTicket(userTickets, firstName, lastName, email)
+		// the num of thread we need to wait for it
+		wg.Add(1)
+		go sendTicket(userTickets, firstName, lastName, email)
+		firstNames := getFirstName()
+		fmt.Printf("These are all our booking: %v\n", firstNames)
+		if RemainingTickets == 0 {
+			fmt.Println("Our conference is booked out. Come back next year.")
+			//break
+		}
+	} else {
+		if !isValidName {
+			fmt.Println("Your entered Name is very short")
+		}
+		if !isValidEmail {
+			fmt.Println("your email address is not correct")
+		}
+		if !isValidTicketNumber {
+			fmt.Printf("We have only %v tickets remaining, so you can't book %v tickets\n", RemainingTickets, userTickets)
 		}
 	}
+	//}
+	// after finish of the all work before exit the program we wait for the threads
+	wg.Wait()
 }
 
 func getFirstName() []string {
@@ -100,4 +107,6 @@ func sendTicket(userTickets uint, firstName, lastName, email string) {
 	fmt.Println("############")
 	fmt.Printf("Sending ticket:\n %v\n to email address%v\n", ticket, email)
 	fmt.Println("############")
+	// after send the email we can say the threads work is finish
+	wg.Done()
 }
