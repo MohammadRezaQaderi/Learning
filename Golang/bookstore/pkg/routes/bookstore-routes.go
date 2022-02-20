b02234f1c2a5e468ccb4c40bd57838a4b8798b6d
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-02-20 13:56:00 +0330

    we config some first background of for the bookstore

---
+++ Golang/bookstore/pkg/routes/bookstore-routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import "github.com/gorilla/mux"
+
+func RegisterBookStoreRoutes(router *mux.Router) {
+	router.HandleFunc("/book/", controller.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controller.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controller.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controller.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controller.DeleteBook).Methods("DELETE")
+
+}
