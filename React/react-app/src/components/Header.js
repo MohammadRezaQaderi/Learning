5fd917cdc276ae271147eefd62abd76b7b4163da
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-03-04 12:17:21 +0330

    this is dynamic style that is not good:

--- React/react-app/src/components/Header.js
+++ React/react-app/src/components/Header.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 const Header = ({title}) => {
   return (
     <header>
-        <h1>{title}</h1>
+        <h1 style={headerstyle}>{title}</h1>
     </header>
   )
 }
@@ -13,5 +13,10 @@ Header.defaultProps = {
 
 Header.prototype ={
     title: PropTypes.string,
+}
+
+const headerstyle = {
+    color: 'red',
+    backgroundColor: "black",
 }
 export default Header
