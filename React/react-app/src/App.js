9cacf92a6c0e39a78737d5a083230bb403af22f1
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-03-04 11:50:51 +0330

    the class component

--- React/react-app/src/App.js
+++ React/react-app/src/App.js
@@ -1,11 +1,16 @@
 import Header from "./components/Header";
+import React from "react";
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return <h1>This is class model</h1>
+  }
 }
-
 export default App;
