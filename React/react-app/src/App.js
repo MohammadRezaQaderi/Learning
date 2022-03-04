9190527c1682549c0d53d7ad1813af5123d3cc82
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-03-04 15:05:11 +0330

    the use of func through by app.js to other copmonents

--- React/react-app/src/App.js
+++ React/react-app/src/App.js
@@ -18,14 +18,17 @@ function App() {
         reminder: true,
     },
   ])
+  const deleteTask = (id) => {
+    console.log('deleted ', id);
+  }
   return (
       <div className="App">
         <Header />
-        <Tasks tasks={tasks}/>
+        <Tasks tasks={tasks} onDelete = {deleteTask}/>
       </div>
     );
 }
-
+  
 // class App extends React.Component{
 //   render(){
 //     return <h1>This is class model</h1>
